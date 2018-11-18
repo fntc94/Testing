156ea64aff7d9d55e99c1b9cfb1c40576f3f9610
franco <fntc94@gmail.com>
2018-11-17 21:55:34 -0300

    Prueba de modificar aldeano

--- fiuba-7507-TP2-2doCuatri2018-master/src/atenea/fiuba/algoIII/ageoOfEmpires/Aldeano.java
+++ fiuba-7507-TP2-2doCuatri2018-master/src/atenea/fiuba/algoIII/ageoOfEmpires/Aldeano.java
@@ -4,7 +4,7 @@ import java.util.function.Consumer;
 
 public class Aldeano implements IRecolectorOro, IConstructor, IReparador {
 
-    private int nuevaVariable = 100;
+    private int nuevaVariable = 1000000;
     private int _vidaMaxima = 50;
     private EdificiosEnConstruccionFabrica _fabricaDeEdificios;
     private int _vidaActual;
